0343dc4f231562482b45d366b2a04e5d496d27fd
Tom Mastwyk <mastwyk42@gmail.com>
2017-08-08 01:36:01 +1000

    using lenses

--- src/TronBot.hs
+++ src/TronBot.hs
@@ -1,7 +1,34 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module TronBot where
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
+import Control.Lens
+
+data Tile = Tile {
+  _x  :: Int,
+  _y :: Int
+} deriving (Eq, Show)
+
+data Grid = Grid {
+  _tiles    :: [Picture],
+  _active   :: Tile,
+  _lastPos  :: Tile
+} deriving (Show)
+
+-- TODO replace grid world reference with state
+data State = State {
+  grid           :: Grid,
+  currentPos     :: (Int, Int),
+  lastP          :: (Int, Int),
+  nextDirection  :: Direction
+}
+
+data Direction = Up' | Down' | Left' | Right'
+
+makeLenses ''Grid
+makeLenses ''Tile
 
 -- TODO move below to config.hs file
 gridSizeX = 800
@@ -24,14 +51,6 @@ main :: IO ()
 main = let window = InWindow "TronBot" windowSize (0, 0)
        in play window black fps initialGrid drawGrid inputHandler frameHandler
 
-data State = State { grid :: Grid
-                   , currentPos :: (Int, Int)
-                   , lastP :: (Int, Int)
-                   , nextDirection :: Direction
-                   }
-
-data Direction = Up' | Down' | Left' | Right'
-
 initialState :: State
 initialState = State initialGrid startPos startPos Up'
 
@@ -39,17 +58,18 @@ initialGrid :: Grid
 initialGrid = newGrid startPos startPos
 
 drawGrid :: Grid -> Picture
-drawGrid (Grid {tiles = t}) = Pictures t
+drawGrid grid = Pictures $ grid^.tiles
 
 inputHandler :: Event -> Grid -> Grid
 inputHandler (EventKey (SpecialKey key) Down _ _) grid = move key grid
 inputHandler _ grid = grid
 
 frameHandler :: Float -> Grid -> Grid
-frameHandler _ (Grid {active = (x, y),
-                      lastPos = (x', y')}) = newGrid (nextPos x x' gridWidth,
-                                                      nextPos y y' gridHeight)
-                                                      (x, y)
+frameHandler _ grid = newGrid newPos (act^.x, act^.y) -- TODO use lens set instead
+                      where act    = grid^.active
+                            last   = grid^.lastPos
+                            newPos = ( nextPos (act^.x) (last^.x) gridWidth,
+                                       nextPos (act^.y) (last^.y) gridHeight)
 
 nextPos :: Int -> Int -> Int -> Int
 nextPos curPos oldPos limit
@@ -58,46 +78,54 @@ nextPos curPos oldPos limit
   where newPos = curPos + (curPos - oldPos)
 
 move :: SpecialKey -> Grid -> Grid
-move key (Grid {active = (x, y), lastPos = (x', y')})
-    | key == KeyDown  && y - y' /= (-1)  && y > 0            = newGrid (x, y-1) (x, y)
-    | key == KeyUp    && y - y' /= 1     && y < gridHeight-1 = newGrid (x, y+1) (x, y)
-    | key == KeyLeft  && x - x' /= (-1)  && x > 0            = newGrid (x-1, y) (x, y)
-    | key == KeyRight && x - x' /= 1     && x < gridWidth-1  = newGrid (x+1, y) (x, y)
-    | otherwise                                              = newGrid (x, y) (x', y')
+move key grid
+    | key == KeyDown  && (abs ay - ly) /= 1  && ay > 0            = newGrid (ax, ay-1) (ax, ay) -- TODO use lens set instead
+    | key == KeyUp    && (abs ay - ly) /= 1  && ay < gridHeight-1 = newGrid (ax, ay+1) (ax, ay)
+    | key == KeyLeft  && (abs ax - lx) /= 1  && ax > 0            = newGrid (ax-1, ay) (ax, ay)
+    | key == KeyRight && (abs ax - lx) /= 1  && ax < gridWidth-1  = newGrid (ax+1, ay) (ax, ay)
+    | otherwise                                                   = grid
+    where ax = grid^.active.x
+          ay = grid^.active.y
+          lx = grid^.lastPos.x
+          ly = grid^.lastPos.y
 
 -- TODO move below to common.hs
 mapTuple2 :: (a -> b) -> (a, a) -> (b, b)
 mapTuple2 f (a1, a2) = (f a1, f a2)
 
 -- TODO move below to grid.hs
-data Grid = Grid { tiles :: [Picture]
-                 , active :: (Int, Int)
-                 , lastPos :: (Int, Int)
-                 } deriving(Eq, Show)
 
 newGrid :: (Int, Int) -> (Int, Int) -> Grid
-newGrid active lastPos = Grid (gridTiles active) active lastPos
+newGrid active lastPos = Grid (gridTiles (newTile active))
+                              (newTile active)
+                              (newTile lastPos)
+
+posToTile :: (Float, Float) -> Tile
+posToTile t = newTile $ posToIndex t
 
 posToIndex :: (Float, Float) -> (Int, Int)
 posToIndex = mapTuple2 (\pos -> round $ pos / tileSize)
 
-gridTiles :: (Int, Int) -> [Picture]
-gridTiles activeIndex = [ Translate
+newTile :: (Int, Int) -> Tile
+newTile (x, y) = Tile x y
+
+gridTiles :: Tile -> [Picture]
+gridTiles activeTile = [ Translate
                           (offset (fst pos) gridSizeX)
                           (offset (snd pos) gridSizeY)
-                          $ gridTile $ activeIndex == posToIndex pos
+                          $ gridTile $ activeTile == posToTile pos
                           | pos <- tilePositions ]
                           where offset pos gridSize = pos - gridSize / 2 + tileSize / 2
                                 tilePositions = [ (x * tileSize, y * tileSize)
                                                   | x <- [0..(gridSizeX / tileSize - 1)]
                                                   , y <- [0..(gridSizeY / tileSize - 1)] ]
 
-emptyTile :: Picture
-emptyTile = Color white $ rectangleWire tileSize tileSize
+emptyTilePic :: Picture
+emptyTilePic = Color white $ rectangleWire tileSize tileSize
 
-activeTile :: Picture
-activeTile = Color green $ rectangleSolid tileSize tileSize
+activeTilePic :: Picture
+activeTilePic = Color green $ rectangleSolid tileSize tileSize
 
 gridTile :: Bool -> Picture
-gridTile True = activeTile
-gridTile False = emptyTile
+gridTile True = activeTilePic
+gridTile False = emptyTilePic
